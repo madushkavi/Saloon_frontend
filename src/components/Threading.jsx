1ed3b2cf17ab07404280ecc1ce7f0e8262936899
JayaniUdara <jayaniudara1998wa22@gmail.com>
2023-11-06 15:14:51 +0530

    updated services

--- src/components/Threading.jsx
+++ src/components/Threading.jsx
@@ -1,38 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/Threading.css";
 import threading from "../assets/threading.png";
 
-const threadingServices = [
-  { service: "Eyebrow ", price: "Rs.200" },
-  { service: "Upper Lip", price: "Rs.50" },
-  { service: "Forehead", price: "Rs.50" },
-  { service: "Chin", price: "Rs.50 upwards" },
-  { service: "Full Face", price: "Rs.500" },
-
-];
-
 const Threading = () => {
+  const [services, setServices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/servicesview")
+      .then((response) => {
+        setServices(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const ThreadingServices = services.filter(
+    (service) => service.serviceType === "threading"
+  );
+
+
   return (
     <div>
      <div className="container" style={{ backgroundImage: `url(${threading})` }}>
      <h1>Threading Services</h1>
      </div>
+     {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div>
       <table>
         <thead>
           <tr>
-            <th>Service</th>
+            <th>Service Name</th>
             <th>Price</th>
           </tr>
         </thead>
         <tbody>
-          {threadingServices.map((item, index) => (
-            <tr key={index}>
-              <td>{item.service}</td>
-              <td>{item.price}</td>
+          {ThreadingServices.map((service) => (
+            <tr key={service.id}>
+              <td>{service.serviceName}</td>
+              <td>{service.price}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      </div>
+      )}
     </div>
   );
 };
